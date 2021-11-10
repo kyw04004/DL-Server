a199f56c679c6ec3781ca3a7e43338a530e2e845
rivermt <rivermt1998@gmail.com>
2021-11-10 16:35:12 +0900

    add allocate

--- src/main/java/com/nerdnull/donlate/server/controller/FinanceController.java
+++ src/main/java/com/nerdnull/donlate/server/controller/FinanceController.java
@@ -1,12 +1,16 @@
 package com.nerdnull.donlate.server.controller;
 
+import com.nerdnull.donlate.server.controller.request.AllocateRequest;
 import com.nerdnull.donlate.server.controller.request.ExchangeRequest;
 import com.nerdnull.donlate.server.controller.response.GetPaymentListResponse;
 import com.nerdnull.donlate.server.controller.response.Response;
 import com.nerdnull.donlate.server.dto.ExchangeDto;
 import com.nerdnull.donlate.server.dto.PaymentDto;
+import com.nerdnull.donlate.server.dto.PlanDto;
+import com.nerdnull.donlate.server.dto.PlanStateDto;
 import com.nerdnull.donlate.server.service.ExchangeService;
 import com.nerdnull.donlate.server.service.PaymentService;
+import com.nerdnull.donlate.server.service.PlanService;
 import com.nerdnull.donlate.server.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,12 +27,14 @@ public class FinanceController {
     private final ExchangeService exchangeService;
     private final PaymentService paymentService;
     private final UserService userService;
+    private final PlanService planService;
 
     @Autowired
-    public FinanceController(ExchangeService exchangeService, PaymentService paymentService, UserService userService) {
+    public FinanceController(ExchangeService exchangeService, PaymentService paymentService, UserService userService, PlanService planService) {
         this.exchangeService = exchangeService;
         this.paymentService = paymentService;
         this.userService = userService;
+        this.planService = planService;
     }
 
     @PostMapping("/exchange")
@@ -82,8 +88,43 @@ public class FinanceController {
     }
 
     @PostMapping("/allocate")
-    public Response<String> allocate(@RequestBody Long planId){
+    public Response<String> allocate(@RequestBody AllocateRequest request){
+        try {
+            request.isNotNull();
+            PlanDto plan = this.planService.getDetails(request.getPlanId());
+            Long deposit = plan.getDeposit();
+            List<PlanStateDto> planStateList = plan.getPlanStateList();
+            Long lateToNormal = deposit * plan.getLatePercent() / 100;
+            Long absentToNormal = deposit * plan.getAbsentPercent() / 100;
+            Long forNormalPerson = 0L;
+            int normalCnt = 0;
 
+            for (PlanStateDto p : planStateList) {
+                Integer lateState = p.getLateState();
+                if (lateState == 1) forNormalPerson += lateToNormal;
+                else if (lateState == 2) forNormalPerson += absentToNormal;
+                else normalCnt++;
+            }
+
+            Long toNormalPerson = (forNormalPerson / normalCnt);
+
+            for (PlanStateDto p : planStateList) {
+                Integer lateState = p.getLateState();
+                Long userId = p.getUserId();
+                if (lateState == 1) this.userService.updatePoint(userId, deposit - lateToNormal);
+                else if (lateState == 2) this.userService.updatePoint(userId, deposit - absentToNormal);
+                else userService.updatePoint(userId, deposit+toNormalPerson);
+            }
+        }
+
+        catch (IllegalArgumentException e){
+            log.error(e.getMessage(), e);
+            return Response.error(Response.BAD_REQUEST, e.getMessage());
+        }
+        catch (Exception e){
+            log.error(e.getMessage(), e);
+            return Response.error(Response.INTERNAL_SERVER_ERROR, e.getMessage());
+        }
         return Response.ok("allocate complete");
     }
 }
--- src/main/java/com/nerdnull/donlate/server/service/PaymentService.java
+++ src/main/java/com/nerdnull/donlate/server/service/PaymentService.java
@@ -1,12 +1,9 @@
 package com.nerdnull.donlate.server.service;
 
 import com.nerdnull.donlate.server.domain.PaymentEntity;
-import com.nerdnull.donlate.server.domain.PlanEntity;
-import com.nerdnull.donlate.server.domain.PlanStateEntity;
 import com.nerdnull.donlate.server.dto.PaymentDto;
 import com.nerdnull.donlate.server.mapper.PaymentMapper;
 import com.nerdnull.donlate.server.repository.PaymentRepository;
-import com.nerdnull.donlate.server.repository.PlanRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.mapstruct.factory.Mappers;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,13 +15,11 @@ import java.util.List;
 @Service
 public class PaymentService {
     private final PaymentRepository paymentRepository;
-    private final PlanRepository planRepository;
     private final PaymentMapper paymentMapper = Mappers.getMapper(PaymentMapper.class);
 
     @Autowired
-    public PaymentService(PaymentRepository paymentRepository, PlanRepository planRepository) {
+    public PaymentService(PaymentRepository paymentRepository) {
         this.paymentRepository = paymentRepository;
-        this.planRepository = planRepository;
     }
 
     public List<PaymentDto> findByUserId(Long userId) {
@@ -39,29 +34,4 @@ public class PaymentService {
     public void delete(Long userId) {
         this.paymentRepository.deleteAllByUserId(userId);
     }
-    public void allocatePoint(Long planId){
-        PlanEntity plan = planRepository.getById(planId);
-        Integer latePercent = plan.getLatePercent();
-        Integer absentPercent = plan.getAbsentPercent();
-        Long deposit = plan.getDeposit();
-        List<PlanStateEntity> planStateList = plan.getPlanStateList();
-        int lateCnt = 0;
-        int absentCnt = 0;
-        int normalCnt = 0;
-
-        for (PlanStateEntity p : planStateList) {
-            Integer lateState = p.getLateState();
-            if (lateState == 1)lateCnt++;
-            else if (lateState == 2)absentCnt++;
-            else normalCnt++;
-        }
-
-        Long toLatePerson = deposit * latePercent / 100;
-        Long toAbsentPerson = deposit * absentCnt / 100;
-      //  Long toNormalPerson = deposit + ();
-
-
-
-    }
-
 }
